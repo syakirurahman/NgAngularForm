5c2308df04a1a297e76b3defdbd4c9cfbc23209e
sofco <sofco@sofco20180702.sofco>
2019-03-15 17:19:33 +0700

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { DataviewComponent } from './dataview/dataview.component';
@@ -12,7 +12,8 @@ import { DataviewComponent } from './dataview/dataview.component';
     DataviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
